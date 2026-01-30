a65661d9520b6902c33c663cea8624b206824f1e
Sam Lopez <samlopez575s@gmail.com>
2026-01-30 10:24:24 -0500

    Se crea una sub clase de la clase Animal llamada pitbull

--- inicio/script.js
+++ inicio/script.js
@@ -20,3 +20,15 @@ const pitbull = new Animal(50, "pitbull");
 console.log(pitbull);
 
 
+
+class Pitbull extends Animal{
+    constructor(tamaño, raza, nombre){
+        super(tamaño, raza);
+        this.nombre = nombre;
+    }
+}
+
+
+const pancho = new Pitbull(23, "pitbull", "pancho");
+
+console.log(pancho);
